5664e510fab6d411224d3b7e145b25bf2e7b35ba
SUDIP10K89 <sudip10k89@gmail.com>
2025-01-30 20:05:39 +0545

    implement service worker and update manifest for PWA functionality

--- js/index.js
+++ js/index.js
@@ -79,3 +79,16 @@ restart.onclick = ()=>{
   obstacle.classList.add('obstacle');
   scoreValue = 0;
 };
+
+//Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((registration) => {
+        console.log('ServiceWorker registration successful');
+      })
+      .catch((err) => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
