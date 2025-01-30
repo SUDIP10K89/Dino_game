544841e8eb3894b1e8467f4522eb3c4cfc948366
SUDIP10K89 <sudip10k89@gmail.com>
2025-01-30 20:14:06 +0545

    update service worker to cache additional assets and improve code formatting

--- sw.js
+++ sw.js
@@ -1,22 +1,25 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
+const CACHE_NAME = "my-pwa-cache-v1";
 const urlsToCache = [
-  '/',
-  '/index.html',
-  './css/index.css',
-  './js/index.js',
-  '/logo.png',
+  "/",
+  "/index.html",
+  "./css/index.css",
+  "./js/index.js",
+  "/logo.png",
+  "./css/bg.png",
+  "./css/dino.png",
+  "./css/cactus.png",
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
+    caches
+      .match(event.request)
       .then((response) => response || fetch(event.request))
   );
 });
